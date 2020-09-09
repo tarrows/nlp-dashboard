a284941ef1edf4a0d8d86ef972b6d63c5f319e71
c10t <c10t@fa.ke>
2020-09-09 23:57:11 +0900

    add fields

--- lib/models.py
+++ lib/models.py
@@ -5,6 +5,10 @@ from tortoise import fields
 class Story(Model):
     id = fields.IntField()
     title = fields.TextField()
+    url = fields.TextField()
+    score = fields.IntField()
+    by = fields.TextField()
+    time = fields.IntField()
 
     def __str__(self):
         return f'{self.id} {self.title}'
